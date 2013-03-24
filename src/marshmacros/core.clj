214a2b02442bbb9332d905e9bfa2807d45f74887
micmarsh <michael.marsh42@gmail.com>
2013-03-24 18:12:33 -0400

    Made sure things were added

---
+++ src/marshmacros/core.clj
@@ -0,0 +1,14 @@
+(ns marshmacros.core
+    (:use [marshmacros.test :only [defntest]]))
+
+(defn -main [& args]
+    (defntest woot { [3] "woot 3"} [thing] (str "woot " thing ))
+    (println (woot 4))
+    (defntest addThreeThings
+        {[1 2 3] 6
+         [3 1 4] 8
+         [0 6 7] 13}
+         [x y z]
+        (+ x y z))
+    (println (addThreeThings 1 2 6)))
+
---
+++ src/marshmacros/test.clj
@@ -0,0 +1,29 @@
+(ns marshmacros.test)
+
+;to abstract more: fn
+
+(defn- throw-exception! [args, result]
+    (throw (Exception. (str "Test Case Failed:\n"
+        "arguments " args " don't produce "
+        " result " result \newline))))
+
+(defn- test-and-throw! [fn-to-apply, input, expected-value]
+    (if (not= expected-value (apply fn-to-apply input))
+        (throw-exception! (seq input) expected-value)))
+
+(defn- loop-through-tests! [fn-to-apply, tests]
+    (loop [allkeys (keys tests)]
+        (if-not (empty? allkeys)
+            (let [input (first allkeys)
+                 expected-value (tests input)]
+                 (test-and-throw! fn-to-apply input expected-value)
+                 (recur (rest allkeys))))))
+
+(defn- get-tests-and-args [first, second]
+    (if (map? first) [first, second] [second, first]))
+
+(defmacro defntest [name  first second & body]
+    (let [  [tests args] (get-tests-and-args first second)
+            result-symbol `(fn [~@args] ~@body) ]
+        (loop-through-tests! (eval result-symbol) tests)
+        `(def ~name ~result-symbol)))
