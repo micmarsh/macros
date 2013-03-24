fef90957aa8a88a6c1f7f61c1c6ba27aa4a4f277
micmarsh <michael.marsh42@gmail.com>
2013-03-24 15:11:25 -0400

    added some debuggin output, still as clueless as ever as to why keyseq is breaking

--- src/macros/core.clj
+++ src/macros/core.clj
@@ -7,6 +7,6 @@
   (println x "Hello, World!"))
 
 (defn -main [& args]
-    (defntest poo {[1 2] 3} [x y] (+ x y))
+    (defntest poo {[1 2] 3} [x y] (+ x y 4))
     (println (poo 4 9)))
 
--- src/macros/test.clj
+++ src/macros/test.clj
@@ -3,15 +3,16 @@
 (defmacro defntest [name first second & body]
     (let [  [tests, args]
                 (if (map? first) [first, second] [second, first])
-            result `(fn [~@args] ~@body)]
+            result-symbol `(fn [~@args] ~@body)
+            result-fn (eval result-symbol)]
         (loop [allkeys (keys tests)]
-            (println allkeys)
-            (if (not= '() allkeys)
+            (println (type allkeys) \space allkeys \space (first allkeys))
+            (if-not (empty? allkeys)
                 (let [input (first allkeys)
                      expected-value (tests input)]
-                     (if (not= expected-value (apply ~result input))
-                        (throw (Exception. (str "Test Exception:\n"
-                            input " doesn't produce " expected-value))))))
+                     (if (not= expected-value (result-fn 1 2));(apply result-fn input))
+                        (throw (Exception. (str "Test Case Failed:\n"
+                            input " doesn't produce " expected-value \newline))))))
             (recur (rest allkeys)))
-        `(def ~name ~result)))
+        `(def ~name ~result-symbol)))
     ;`(defn ~name [~@args] ~@body)))
