4cd5da583738ea0a7bd588ce4a06dbc40319bdea
micmarsh <michael.marsh42@gmail.com>
2013-03-24 17:27:03 -0400

    broke up into separate functions, added some !s

--- src/macros/test.clj
+++ src/macros/test.clj
@@ -1,15 +1,25 @@
 (ns macros.test)
 
-(defmacro defntest [name tests args & body]
-    (let [  result-symbol `(fn [~@args] ~@body)
-            result-fn (eval result-symbol)]
-        (loop [allkeys (keys tests)]
-            (if-not (empty? allkeys)
-                (let [input (first allkeys)
-                     expected-value (tests input)]
-                     (if (not= expected-value (apply result-fn input))
-                        (throw (Exception. (str "Test Case Failed:\n"
-                            "arugments " (seq input) " don't produce "
-                            " result " expected-value \newline))))
-                     (recur (rest allkeys)))))
+(defn- throw-exception! [args, result]
+    (throw (Exception. (str "Test Case Failed:\n"
+        "arguments " args " don't produce "
+        " result " result \newline))))
+
+
+(defn- test-and-throw! [fn-to-apply, input, expected-value]
+    (if (not= expected-value (apply fn-to-apply input))
+        (throw-exception! (seq input) expected-value)))
+
+(defn- loop-through-tests! [fn-to-apply, tests]
+    (loop [allkeys (keys tests)]
+        (if-not (empty? allkeys)
+            (let [input (first allkeys)
+                 expected-value (tests input)]
+                 (test-and-throw! fn-to-apply input expected-value)
+                 (recur (rest allkeys))))))
+
+(defmacro defntest [name  first second & body]
+    (let [  [tests args] (if (map? first) [first, second] [second, first])
+            result-symbol `(fn [~@args] ~@body) ]
+        (loop-through-tests! (eval result-symbol) tests)
         `(def ~name ~result-symbol)))
