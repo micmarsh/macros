fef18382604e7552824413f560fa18c05639ceaa
micmarsh <michael.marsh42@gmail.com>
2013-06-01 00:58:51 -0400

    Test would have failed anyway

--- src/marshmacros/coffee.clj
+++ src/marshmacros/coffee.clj
@@ -6,9 +6,10 @@
 (def drink "water")
 
 (defntest add-to-map [map-so-far, current-symbol]
-    {[{} fruit] {:fruit :fruit}
+    {[{} fruit] {:fruit fruit}
      [{:meat meat} drink]
         {:meat meat :drink drink}}
+    "Adds the current symbol and it's keyword to the given map"
     (assoc
         map-so-far
         (keyword (name current-symbol))
