6a374d30ba7c5c948928b70e3e10ea23b5a72b8a
micmarsh <michael.marsh42@gmail.com>
2013-06-01 00:49:31 -0400

    Broke it cryptically again, committing for the historical record

--- src/marshmacros/coffee.clj
+++ src/marshmacros/coffee.clj
@@ -1,11 +1,20 @@
-(ns marshmacros.coffee)
+(ns marshmacros.coffee
+    :use [marshmacros.test :only [defntest]])
+
+(def fruit "apples")
+(def meat {:name "beef" :types ["ground", "grass-fed"]})
+(def drink "water")
+
+(defntest add-to-map [map-so-far, current-symbol]
+    {[{} fruit] {:fruit :fruit}
+     [{:meat meat} drink]
+        {:meat meat :drink drink}}
+    (assoc
+        map-so-far
+        (keyword (name current-symbol))
+        current-symbol))
 
 (defmacro cofmap [& symbols]
- (reduce (fn [map-so-far, current-symbol]
-        (assoc
-            map-so-far
-            (keyword (name current-symbol))
-            current-symbol))
-    {} symbols ))
+ (reduce add-to-map {} symbols))
 
 ;(defmacro dcofmap [& symbols])
